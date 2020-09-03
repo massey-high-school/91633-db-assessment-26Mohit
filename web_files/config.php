43dd294aa588db24afdc4050995ca6e9b037aae1
26Mohit <60586130+26Mohit@users.noreply.github.com>
2020-09-03 14:49:17 +1200

    commit

---
+++ web_files/config.php
@@ -0,0 +1,12 @@
+<?php
+
+ini_set('display_errors', 'On');  //change to 'Off' when live
+
+define ("DB_NAME", "negim66919");
+define ("DB_USERNAME", "_negim66919");
+define ("DB_PASSWORD", "e1dfe2db138ccca83849c20fd4ae22f1");
+define ("DB_HOST", "localhost");
+
+define ("IMAGE_DIRECTORY","../images");
+
+?>
