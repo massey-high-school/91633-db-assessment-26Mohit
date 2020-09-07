c4a22bbb1f43ece9f978d1907e86aa3205809c69
26Mohit <60586130+26Mohit@users.noreply.github.com>
2020-09-07 12:16:11 +1200

    commit

---
+++ web_files/content/home.php
@@ -0,0 +1,4 @@
+
+		<h1>Welcome to Te Reo Dictionary</h1>
+
+		<p>Please choose a topic from the list above or <a href="admin/admin.php?php=login">login</a> to access the admin panel.</p>	
---
+++ web_files/theme/bottombit.php
@@ -0,0 +1,8 @@
+    
+    <div class="footer">
+		&copy; Mohit Negi 2020
+	</div> <!-- end footer -->
+
+</div> <!-- end content -->
+
+</body>
