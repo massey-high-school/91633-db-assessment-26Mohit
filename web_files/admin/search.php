8da082f3fd3ec83249ad6e4ac8fc5fc694fdc3f3
26Mohit <60586130+26Mohit@users.noreply.github.com>
2020-09-20 14:33:40 +1200

    commit

--- web_files/admin/search.php
+++ web_files/admin/search.php
@@ -1,9 +1,21 @@
 <?php
 
+$output = '';
+if(isset($_POST['DB_vocab'])) {
+    $DB_vocab = $_POST['DB_vocab'];
+    $DB_vocab = preg_replace('/[^a-zA-Z0-9]/', '',  $DB_vocab);
+	
 	// find the words from the database
-	$DB_vocab_sql="SELECT * From `DB_topic WHERE `word` LIKE ('DB_vocab')";
-    $DB_vocab_query=mysqli_query($dbconnect, $DB_vocab_sql);
+    $DB_vocab_query=mysqli_query("SELECT * From `DB_vocab` WHERE `word` LIKE ('%$DB_vocab%')");
 	$DB_vocab_rs=mysqli_fetch_assoc($DB_vocab_query);
 
+    
+    if($DB_vocab_rs == 0){
+      $output = "There was no search results!";
 
+    }else{
+
+while (mysqli_fetch_array($DB_vocab_query)) {$DB_vocab;}
+$output .= 'DB_vocab';}}
 ?>
+<?php print ("$output"); ?>
